06161380637e3e80b807f230ae98b9961094f9a3
Nguezet Jordan Junior <kenfackjordanjunior@gmail.com>
2024-03-20 08:23:55 +0100

    fixed_socket_endpoint

--- middleware/src/utils/socket-service/socketService.ts
+++ middleware/src/utils/socket-service/socketService.ts
@@ -1,62 +1,64 @@
-import { Server, Socket } from 'socket.io';
-import http from 'http'
+import { Server, Socket } from "socket.io";
+import http from "http";
 
 export default class SocketService {
+  public socket: Socket | null;
+  private activeRooms: {};
+  public io: Server;
+  public server: http.Server;
+  constructor(app: Express.Application) {
+    this.server = http.createServer(app);
+    this.io = new Server(this.server, { cors: { origin: "*" } });
+    this.socket = null;
+    this.activeRooms = {};
+    console.log("============== Socket Server Started =============");
 
-    public socket: Socket | null
-    private activeRooms: {}
-    public io: Server
-    constructor(app: Express.Application) {
-        const server = http.createServer(app)
-        this.io = new Server(server)
-        this.socket = null
-        this.activeRooms = {}
-        console.log("============== Socket Server Started =============");
+    this.io.on("connection", (socket: Socket) => {
+      console.log("=============+> A user connected <+=");
+      this.socket = socket;
+      this.startSocketListeners();
+    });
+  }
 
-        
-        
-        this.io.on('connection', (socket: Socket) => {
-            console.log('=============+> A user connected <+=');
-            this.socket = socket
-            this.startSocketListeners()
-        })
+  private startSocketListeners() {
+    // Join room
+    this.socket?.on("joinRoom", (roomId) => {
+      this.activeRooms[roomId] = [
+        ...(this.activeRooms?.[roomId] || []),
+        this.socket?.id,
+      ];
+      this.socket?.join(roomId);
+      this.socket?.emit("connected", { roomId });
+    });
 
-    }
+    // Handle disconnection
+    this.socket?.on("disconnect", () => {
+      // Loop through active rooms
+      for (const roomId in this.activeRooms) {
+        if (Object.prototype.hasOwnProperty.call(this.activeRooms, roomId)) {
+          // Remove disconnected socket from activeRooms for each room
+          this.activeRooms[roomId] = this.activeRooms[roomId].filter(
+            (socketId) => socketId !== this.socket?.id
+          );
+        }
+      }
+    });
 
-    private startSocketListeners() {
-        // Join room
-        this.socket?.on('joinRoom', (roomId) => {
-            this.activeRooms[roomId] = [...(this.activeRooms?.[roomId] || []), this.socket?.id];
-            this.socket?.join(roomId);
-            this.socket?.emit('connected', { roomId });
-        });
+    this.socket?.on("sendMessage", (data) => {
+      const { roomId, message } = data;
+      console.log("Sending ", message, " to room: ", roomId);
 
-        // Handle disconnection
-        this.socket?.on('disconnect', () => {
-            // Loop through active rooms
-            for (const roomId in this.activeRooms) {
-                if (Object.prototype.hasOwnProperty.call(this.activeRooms, roomId)) {
-                    // Remove disconnected socket from activeRooms for each room
-                    this.activeRooms[roomId] = this.activeRooms[roomId].filter(socketId => socketId !== this.socket?.id);
-                }
-            }
-        });
+      this.io.to(roomId).emit("newMessage", { roomId, message });
+    });
 
-        this.socket?.on('sendMessage', (data) => {
-            const { roomId, message } = data;
-            console.log("Sending ", message," to room: ", roomId);
-            
-            this.io.to(roomId).emit('newMessage', { roomId, message });
-        });
+    this.socket?.on("sendMessage-singleBroadcast", (data) => {
+      const { roomId, message } = data;
+      const roomSockets = this.activeRooms[roomId] || [];
 
-        this.socket?.on('sendMessage-singleBroadcast', (data) => {
-            const { roomId, message } = data;
-            const roomSockets = this.activeRooms[roomId] || [];
-
-            // Iterate over each socket ID in the room and send the message individually
-            roomSockets.forEach(socketId => {
-                this.io.to(socketId).emit('newMessage', { roomId, message });
-            });
-        });
-    }
+      // Iterate over each socket ID in the room and send the message individually
+      roomSockets.forEach((socketId) => {
+        this.io.to(socketId).emit("newMessage", { roomId, message });
+      });
+    });
+  }
 }
